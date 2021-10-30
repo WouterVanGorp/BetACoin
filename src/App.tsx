5e05cb55cbebdfbdf0ae3d8a1b916a84a8999db2
xv-k <xv_k@outlook.com>
2021-10-30 15:18:38 +0200

    Codegenerator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import { Header, Navbar } from "./components";
 import { Board } from "./square/pages/Board";
@@ -18,12 +18,37 @@ const boardGenerator = (numCol = 4, numRow = 4) => {
   return board;
 };
 
+const codeGenerator = (numCol = 4, numRow = 4) => {
+  let codes = [];
+  for (let r = 0; r < numRow; r++) {
+    for (let c = 1; c <= numCol; c++) {
+      codes.push({
+        key: `${c}${String.fromCharCode(65 + r)}`,
+        code: makeCode(10),
+      });
+    }
+  }
+  return codes;
+};
+
+function makeCode(length: number) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 const tileClickHandler = (tile: any) => {
   tile.clicked = true;
 };
-let tilelist = boardGenerator();
 
 function App() {
+  const [tilelist, changeTile] = useState(boardGenerator());
+
   return (
     <div>
       <Header />
