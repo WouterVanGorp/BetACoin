c0a0ce2a926f1c42eb67b29677b1ff2b3b28770c
wouter vg <wouter.vangorp@ordina.be>
2021-10-30 16:17:38 +0200

    lol

--- src/square/pages/Overview.tsx
+++ src/square/pages/Overview.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import styles from "./Overview.module.css";
+
 interface Props {}
 
 export function Overview({}: Props) {
-  return <h1>Overview</h1>;
+  const [games, addGame] = useState(["", ""]);
+
+  const newGame = () => {
+    addGame([...games, ""]);
+  };
+
+  return (
+    <>
+      <h1>Square game</h1>
+      <Button variant="primary" onClick={newGame}>
+        Add Game
+      </Button>
+
+      <div className={styles.cardContainer}>
+        {games.map((_) => (
+          <Card className={styles.card} body>
+            test
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 }
