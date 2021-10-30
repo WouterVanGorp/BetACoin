eab1c5ac7f824ca772757b66bbbb7239fc2d4285
wouter vg <wouter.vangorp@ordina.be>
2021-10-30 11:48:45 +0200

    components

---
+++ src/square/components/Grid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export function Grid({}: Props) {
+  return <h1>compoment</h1>;
+}
---
+++ src/square/pages/Boards.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export function Boards({}: Props) {
+  return <h1>compoment</h1>;
+}
