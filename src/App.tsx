372b680e424eda7328a0cb13a3c7b206684663f7
wouter vg <wouter.vangorp@ordina.be>
2021-10-30 13:01:08 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import "./styles/App.css";
+import { Navbar, Header } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+      <div>
+        <Navbar />
+      </div>
     </div>
   );
 }
