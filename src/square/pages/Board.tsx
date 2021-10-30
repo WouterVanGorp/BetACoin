061e96bc3affbb2ea04d45406436bdd7962f6601
xv-k <xv_k@outlook.com>
2021-10-30 14:18:00 +0200

    test

--- src/square/pages/Board.tsx
+++ src/square/pages/Board.tsx
@@ -4,6 +4,11 @@ interface Props {
 }
 
 export function Board({ bs, onClick }: Props) {
+  const myStyles = {
+    built: { backgroundColor: "rgba(50, 50, 50, 0.8)", color: "#b9b9b9" },
+    unbuilt: { backgroundColor: "rgba(255, 255, 255, 0.8)" },
+  };
+
   return (
     <div className="board-container">
       {bs.map((tile: any) => {
@@ -12,8 +17,9 @@ export function Board({ bs, onClick }: Props) {
             onClick={() => onClick(tile)}
             key={tile.key}
             className="board-item"
+            style={tile.clicked ? myStyles.built : myStyles.unbuilt}
           >
-            {tile.name}
+            ...
           </div>
         );
       })}
