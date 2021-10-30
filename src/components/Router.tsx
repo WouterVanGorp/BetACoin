0f8cce206c2ee9b9450e58f6be84d92fa03d8ff1
wouter vg <wouter.vangorp@ordina.be>
2021-10-30 15:51:12 +0200

    routing basixx

---
+++ src/components/Router.tsx
@@ -0,0 +1,17 @@
+import { Switch, Route } from "react-router-dom";
+import { Routes } from "./../routes";
+import { Login } from "./../login/login";
+import { Overview } from "./../square/pages/Overview";
+
+export function Router() {
+  return (
+    <Switch>
+      <Route path={Routes.LOGIN}>
+        <Login />
+      </Route>
+      <Route path={Routes.GAME}>
+        <Overview />
+      </Route>
+    </Switch>
+  );
+}
