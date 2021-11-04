176b0ce3e579706f9cdedcc058575af904b8814d
wouter vg <wouter.vangorp@ordina.be>
2021-11-04 11:49:11 +0100

    styling

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,24 @@
 import { Switch, Route, Redirect } from "react-router-dom";
+
+import styles from './styles/Router.module.css';
 import { Routes } from "./routes";
 import { Login } from "./login/login";
 import { Overview } from "./square/pages/Overview";
 
 export function Router() {
   return (
-    <Switch>
-      <Route path={Routes.LOGIN}>
-        <Login />
-      </Route>
-      <Route path={Routes.GAME_SQUARE}>
-        <Overview />
-      </Route>
-      <Route path="*">
-        <Redirect to={Routes.GAME_SQUARE} />
-      </Route>
-    </Switch>
+    <div className={styles.routerContainer}>
+      <Switch>
+        <Route path={Routes.LOGIN}>
+          <Login />
+        </Route>
+        <Route path={Routes.GAME_SQUARE}>
+          <Overview />
+        </Route>
+        <Route path="*">
+          <Redirect to={Routes.GAME_SQUARE} />
+        </Route>
+      </Switch>
+    </div>
   );
 }
